78701751d903eaebce677fe878f1478956f65a95
John Reyes <johncqr@gmail.com>
2017-10-05 23:47:21 -0700

    complete random movement AI with backtracking

--- src/MyAI.py
+++ src/MyAI.py
@@ -17,35 +17,66 @@
 #                be lost when the tournament runs your code.
 # ======================================================================
 
+import random
 from Agent import Agent
 
-class MyAI ( Agent ):
+class MyAI(Agent):
 
-    def __init__ ( self ):
+    def __init__(self):
         # ======================================================================
         # YOUR CODE BEGINS
         # ======================================================================
-        
-        pass
+
+        self._has_gold = False
+        self._breadcrumbs = []
+
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
 
-    def getAction( self, stench, breeze, glitter, bump, scream ):
+    def getAction(self, stench, breeze, glitter, bump, scream):
         # ======================================================================
         # YOUR CODE BEGINS
         # ======================================================================
-        
-        return Agent.Action.CLIMB
+
+        if self._has_gold:
+            if not self._breadcrumbs:
+                return Agent.Action.CLIMB
+            # Backtrack
+            return self._breadcrumbs.pop()
+
+        if glitter:
+            self._has_gold = True
+
+            # Turn around
+            self._breadcrumbs.append(Agent.Action.TURN_LEFT)
+            self._breadcrumbs.append(Agent.Action.TURN_LEFT)
+            return Agent.Action.GRAB
+
+        selected_action = self.__actions[random.randrange(len(self.__actions))]
+        if selected_action == Agent.Action.TURN_LEFT:
+            self._breadcrumbs.append(Agent.Action.TURN_RIGHT)
+        elif selected_action == Agent.Action.TURN_RIGHT:
+            self._breadcrumbs.append(Agent.Action.TURN_LEFT)
+        elif selected_action == Agent.Action.FORWARD:
+            self._breadcrumbs.append(Agent.Action.FORWARD)
+
+        return selected_action
+
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
-    
+
     # ======================================================================
     # YOUR CODE BEGINS
     # ======================================================================
 
-    
+    __actions = [
+        Agent.Action.TURN_LEFT,
+        Agent.Action.TURN_RIGHT,
+        Agent.Action.FORWARD,
+    ]
+
     # ======================================================================
     # YOUR CODE ENDS
     # ======================================================================
